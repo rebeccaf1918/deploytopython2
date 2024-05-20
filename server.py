965c45df02b476ce00365b911b50e75b9ddd876f
rebeccaf1918 <123907810+rebeccaf1918@users.noreply.github.com>
2024-05-20 17:39:20 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/', methods=['GET'])
 def getallSongs():
-    return "Hello World!!"
+    return "Hello World!!!"
 
 if __name__ == "__main__":
     app.run(debug=True)
