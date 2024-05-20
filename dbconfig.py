6865e3f76a64676f78a503e6dba5a60707353b1b
Rebecca Feeley <G00425652@atu.ie>
2024-05-20 18:54:48 +0100

    updating dbconfig file

--- dbconfig.py
+++ dbconfig.py
@@ -1,5 +1,5 @@
 mysql = {
-    'host':"https://rebeccaf1918.pythonanywhere.com/",
+    'host':"rebeccaf1918.mysql.pythonanywhere-services.com",
     'user':"rebeccaf1918",
     'password':"RootRoot",
     'database':"rebeccaf1918$taylorsongs"
